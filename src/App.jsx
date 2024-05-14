b41a732ff9e7175b2940ac3c31d8387fecfec9e0
Rohit Kumar Verma <rohitkverma2234@gmail.com>
2024-05-14 19:00:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Toaster } from "react-hot-toast";
+import "./App.css";
+import Todo from "./components/Todo";
+
+function App() {
+  return (
+    <>
+      <Todo />
+    </>
+  );
+}
+
+export default App;
